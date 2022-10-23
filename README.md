97f4af34ec0adcdcee5e4e3041683ac1e2b34970
royenheart <royenheart@outlook.com>
2022-10-23 20:41:14 +0800

    更新任务；加入数据类型大小查看的实验

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # OSExp
 
 OS Experiment
+
+## TODO
+
+- [ ] 使用Astyle格式化
+- [ ] 加入google test
---
+++ functions_test/typeSize.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv) {
+    #ifdef _WIN32
+    const char sys[] = "Windows system";
+    #elif defined(__linux__)
+    const char sys[] = "Linux system";
+    #else
+    const char sys[] = "Other system";
+    #endif
+    
+    printf("%s: char - %d\n", sys, sizeof(char));
+    printf("%s: short - %d\n", sys, sizeof(short));
+    printf("%s: int - %d\n", sys, sizeof(int));
+    printf("%s: long - %d\n", sys, sizeof(long));
+    printf("%s: long int - %d\n", sys, sizeof(long int));
+    printf("%s: float - %d\n", sys, sizeof(float));
+    printf("%s: double - %d\n", sys, sizeof(double));
+}
